bcae50ceaa2dec8124843ba96e83b364023634eb
suraj sakhare <sakharesuraj10@gmail.com>
2024-11-22 13:51:31 +0530

    add some pages

--- client/src/pages/Shipping.jsx
+++ client/src/pages/Shipping.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Shipping = () => {
   return (
-    <div className="max-w-4xl mx-auto p-6 bg-white shadow-lg rounded-lg">
+    <div className="min-h-screen my-20 max-w-4xl mx-auto p-6 bg-white shadow-lg rounded-lg">
       <h1 className="text-3xl font-semibold mb-4">Shipping and Delivery</h1>
       <p className="text-sm text-gray-700">
         Last updated on Nov 22, 2024
